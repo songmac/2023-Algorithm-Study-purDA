0a67c27f5cb694741e756763dcc0bf9e0db7aca0
gmlgml5023 <gmlgml5023@naver.com>
2023-10-19 00:07:52 +0900

    [BOOK-1-15]

--- GaHee/BookStudy/1주차/print_stars2.py
+++ GaHee/BookStudy/1주차/print_stars2.py
@@ -1,12 +1,12 @@
-# 1-14
-# *를 n개 출력하되 w개마다 줄바꿈하기 (2)
+# 1-15
+## *를 n개 출력하되 w개마다 줄바꿈하기 (2)
 
 print('*를 출력합니다.')
 
 n = int(input('몇 개를 출력할까요? : '))
 w = int(input('몇 개마다 줄바꿈할까요 : '))
 
-for _ in range(n//w):
+for _ in range(n // w):
    print('*' * w)
 
 rest = n % w
