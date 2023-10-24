42389d7d79404234c4c5e64d18169bbf1225cb5a
LeeJiwoo982 <easywoo980902@gmail.com>
2023-10-25 02:05:22 +0900

    [BOOK-2-7[A]]함수 작동 확인

--- JiWoo/BookStudy/2week/2-7A.py
+++ JiWoo/BookStudy/2week/2-7A.py
@@ -11,3 +11,5 @@ def card_conv(x: int, r:int) -> str : # x,r 인수형 넣은 함수 스트링으
         x //= r
         # x를 r로 계속 나눈 나머지를 x에 대입하다 x가 0이하 될 때 끝!
     return d[::-1] #역순으로 반환
+
+print(card_conv(283709153,31)) #결과 확인.
