09bd8b521f702ca3868fe6a2a8de0d43a940679e
Suyeon <ssy4784@naver.com>
2023-10-29 00:55:59 +0900

    update : file

--- SuYeon/BookStudy/문제랜덤지정.py
+++ SuYeon/BookStudy/문제랜덤지정.py
@@ -1,21 +1,24 @@
 import random
 
-def BookStudy(prob_num) :
+def BookStudy(num) :
+  if num > 0 :
+    prob = random.sample(range(1,num),7) #문제 7개 랜덤 뽑기
+    mem = ['가희', '수연', '유미', '소희', '지우', '하영', '윤선']
+    prob_mem_set = list(zip(prob, mem))
 
-  prob_extract = random.sample(range(1,prob_num),7) #문제 7개 랜덤 뽑기
-  members = ['가희', '수연', '유미', '소희', '지우', '하영', '윤선']
-  prob_mem_set = list(zip(prob_extract, members))
+    for result in prob_mem_set :
+      print(result)
+      
+    return result
 
-  for show in prob_mem_set :
-    print(show)
-
-  return show
-
-prob_num= int(input('문제 수를 입력하세요: '))
-result = BookStudy(prob_num)
-print(result)
+  else :
+    return print("문제 수를 잘못 입력하였습니다.")
 
 
+
+chap= int(input('단원: '))
+num= int(input('문제 수를 입력하세요: '))
+result = BookStudy(num)
 
 
 
